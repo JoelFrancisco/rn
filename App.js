dd41139638afaccc0e57a79ed897f59bff33e279
Joel Francisco <joelfranciscofilho@gmail.com>
2021-04-06 17:26:33 -0300

    feat: adding project

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.teste}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
